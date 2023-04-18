8e4016148c4abd3f8a11abcf04e4ec64d4807e21
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2023-04-18 06:52:30 +0200

    Fix types definition for python 3.8

--- chili/state.py
+++ chili/state.py
@@ -2,6 +2,6 @@ from __future__ import annotations
 
 from typing import Dict, Union
 
-StatefulTypes = Union[list, str, int, float, bool, dict[str, ...], tuple]  # type: ignore
+StatefulTypes = Union[list, str, int, float, bool, Dict[str, 'StatefulTypes'], tuple]  # type: ignore
 
-StateObject = Union[None, StatefulTypes, Dict[str, StatefulTypes]]
+StateObject = Union[None, StatefulTypes, Dict[str, StatefulTypes]]  # type: ignore[misc]
